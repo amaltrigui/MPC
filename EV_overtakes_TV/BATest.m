3eed0a8484cc16ae54daf1104bb9e69002dce532
amal trigui <50722211+amaltrigui@users.noreply.github.com>
2021-11-04 11:51:12 +0100

    Add files via upload

---
+++ EV_overtakes_TV/BATest.m
@@ -0,0 +1,321 @@
+function Main
+%% General
+    clc;
+    close all;
+    clear all;
+    addpath myfunctionsfolder;
+    
+    figure(1);
+        title('Test Two Cars on the same Lane ');
+        xlabel(' x in meter');
+        ylabel(' y in meter');
+        grid on;
+        hold on;
+        
+        
+    mpciterations = 60;     % the Length of the Simulation 
+    N             = 12;      % prediction horizon  
+    T             = 0.2;    % Sampling interval
+    
+
+%% EV and Lane Properties_Objeters
+    Properties_Obj.l_r = 2;    
+    Properties_Obj.l_f = 2;    
+    
+    % Matrices for(Cost function) from the Paper (see Page 6/7)
+    Properties_Obj.Q = diag([0 1 1 1]);
+    Properties_Obj.R = diag([0.33 5]);
+    Properties_Obj.S = diag([0.33 15]);
+
+    Properties_Obj.v_max = 20; 
+
+    Properties_Obj.Lane = [-4,4]; 
+
+    Properties_Obj.maxDeviation = 4;
+    Properties_Obj.safety_distance = 2; % min Radius between two cars
+
+%% Initial values
+    % Initial state X: (4*1)Vector (s,d,phi,v)
+    
+    % EV:
+    % Velocity of the EV is 7m/S
+    xmeasure      = [0.0; -2.0; 0.0; 7];  
+    
+    % Input:
+    u0            = zeros(2,N);  
+    
+    % TV:
+    % Velocity of Car 2 is 5m/S, smaller than velocity of EV --> to get the
+    % Accident-Alarm Case. TV is already at the Beginning of the Simulation
+    % 8 m ahead of EV.
+    xTVmeasure_0  = [8,-2,0,5];  
+       
+%% reference trajectory
+
+    % EV:
+    % How is it wished, so that the car goes if no constaints are added 
+    %              --> d 'lateral position' would stay 0(No deviation)
+    %              --> phi 'Orientation' is also 0
+    %              --> v would stay kostant 7 m/s
+    %              --> s 'longitudinal position' goes from 0:v*t and t is 1 second for exemple ? Does
+    %              it matter since Q(0) =0000--> No
+    d           =   xmeasure(2)*ones(1,mpciterations+N+1);
+    phi         =   zeros(1,mpciterations+N+1);
+    v           =   xmeasure(4)*ones(1,mpciterations+N+1);
+    x_ref = [0:mpciterations+N;d;phi;v];
+    
+    % TV:
+    % Reference Trajectory of TV
+    % assuming that the second car maintains the same velocity and same lane
+    dTV         =   xTVmeasure_0(2)*ones(1,mpciterations+N+1);
+    phiTV       =   zeros(1,mpciterations+N+1);
+    vTV         =   xTVmeasure_0(4)*ones(1,mpciterations+N+1);
+    sTV         =   (0:1:mpciterations+N)*T*xTVmeasure_0(4)+ xTVmeasure_0(1);
+    x_refTV     =   [sTV;dTV;phiTV;vTV];   
+    
+%% Optimization
+
+    Properties_Obj.steps = 1;
+    nmpc(@runningcosts, @constraints, ...
+          @linearconstraints, @system, ...
+         mpciterations, N, T, xmeasure, u0, x_ref,x_refTV,@Determine_X_new, ...
+            Properties_Obj, @computeCurvedBicycleModel, @printHeader, @printClosedloopData, @plotTrajectories);
+
+    rmpath('./myfunctionsfolder');
+
+end
+
+
+function cost = runningcosts(x, u, x_ref,Properties_Obj)
+%% From the Paper , see Formel (25)
+
+   u_prev = u(:,1);
+   u_curr = u(:,2);
+  
+   cost = (x-x_ref).'*Properties_Obj.Q*(x-x_ref) + u_curr.'*Properties_Obj.R*(u_curr) + (u_curr-u_prev).'*Properties_Obj.S*(u_curr-u_prev);
+end
+
+
+function [c,ceq] = constraints(x, x_ref,x_refTV, k, Properties_Obj, accident)
+%% constraints
+    
+    % velocity 
+    vmax = x(4)- Properties_Obj.v_max ;  % v<vmax
+    v_positive = -x(4);        % v>0
+    
+    
+    % do not deviate so much from reference Trajectory 
+    maxDev = Properties_Obj.maxDeviation; 
+    R = abs( x(2)-x_ref(2) )-maxDev;
+    
+    % do not decelearte or accelerate too much 
+    V= abs (x(4)-x_ref(4)) - 0.5 ;
+    
+    % last Element of c is -2, which is always negative but in case of
+    % accident detected it changes to the constraint that prevent it
+    c   = [vmax; v_positive ;V; R ; -2];
+
+    width_car = (Properties_Obj.l_f+Properties_Obj.l_r)/2;
+    if(accident(k)==1)
+        A = x_refTV(2)+width_car+Properties_Obj.safety_distance- x(2);
+        c(end)=A;
+    end
+          
+    ceq = [];
+end
+
+function [A, b, Aeq, beq, lb, ub] = linearconstraints()
+%% Input Constraints
+    A   = [];
+    b   = [];
+    Aeq = [];
+    beq = [];
+    lb  = [-9; -0.52];   % Min input bound (paper)
+    ub  = [5;0.52];      % Max input bound
+end
+
+
+
+function x_new = Determine_X_new(x, u, Properties_Obj)
+ % formel (18) Paper
+ 
+ s = x(1); d = x(2); phi = x(3); v = x(4); a = u(1); delta = u(2);
+  
+ l_r = Properties_Obj.l_r;  l_f = Properties_Obj.l_f;
+ 
+ alpha = atan((l_r*tan(delta))/(l_f + l_r));
+ s_new = v*cos(phi + alpha);
+ d_new = v*sin(phi + alpha);
+ phi_new = (v*sin(alpha))/l_r;
+ v_new = a;
+
+ x_new = [s_new;d_new;phi_new;v_new];
+
+end
+
+
+ function y = system(x, u, T, x_curr, myMatrices)
+ %% system Equation
+    A_d = myMatrices.A;
+    B_d = myMatrices.B;
+    x0fcT = myMatrices.x0fcT;
+
+    y = x0fcT + A_d*(x - x_curr) + B_d*u;
+ end
+ 
+
+ %% print results
+ function printHeader()
+    fprintf('   k  |      a(k)        delta(k)        s        d        phi        v     Time\n');
+    fprintf('--------------------------------------------------------------------------------\n');
+end
+
+function printClosedloopData(mpciter, u, x, t_Elapsed)
+% from Web
+    fprintf(' %3d  | %+11.6f %+11.6f %+11.6f %+11.6f %+11.6f %+11.6f %+6.3f \n', ...
+             mpciter, u(1,1),u(2,1), x(1), x(2),x(3),x(4), t_Elapsed);
+end
+
+
+function plotTrajectories( x, x0, x_ref, Properties_Obj,x_TV)
+    
+    % Assuming that the Width and Length of EV is equal to TV
+    l_r = Properties_Obj.l_r;
+    l_f = Properties_Obj.l_f;
+    L = l_r + l_f;  %  length of car
+    B = L/2;        %  width of car
+
+    C = [0. 0. ; L 0. ; L B ; 0. B; 0 0.] ; % Car Center coordinates ( from Internet copied--> stil need to check it butit  works )
+
+    y_lim = Properties_Obj.Lane;
+    y_middle = (y_lim(1)+y_lim(2))/2; % lane cut in 2
+
+
+    % plot street
+    plot(linspace(-2,90,100),y_lim(1)*ones(100),'b')  % upper Borderie
+    plot(linspace(-2,90,100),y_lim(2)*ones(100),'b')  % lower Borderie
+    plot(linspace(-2,90,100),y_middle*ones(100), '--')    % middle of Lane
+
+    % plot x_Ref EV
+    plot(linspace(x_ref(1,1),x_ref(1,length(x_ref)),100),linspace(x_ref(2,1),x_ref(2,length(x_ref)), 100),'r')
+    
+    % plot EV trajectory
+    h8= plot(C(:,1)+x0(1)-L/2,C(:,2)+x0(2)-B/2,'black');
+
+    % plot TV trajectory
+    h9 =plot(C(:,1)+x_TV(1)-L/2,C(:,2)+x_TV(2)-B/2,'green');
+
+    axis([-2 90 -35 35]); 
+    pause(0.3)
+    set(h8,'Visible','off')
+    set(h9,'Visible','off')
+
+end
+
+
+%% CurvedBicycleModel Paper/Tomasso
+function [Ad, Bd, x0fcT] = computeCurvedBicycleModel(x, T, k, lf, lr)
+
+    d = x(2);
+    v = x(4);
+    c0 = cos(x(3));
+    s0 = sin(x(3));
+    alpha1 = lr/(lr+lf);
+    
+    w0 = 1-k*d;
+    w1 = 1/w0;
+    lambda = v*k*w1;
+
+    z0 = sqrt(1-5*c0^2);  %   z1-z2=2*z0
+    z1 = s0+z0;
+    z2 = s0-z0;
+    e1 = exp(lambda*z1*T/2);
+    e2 = exp(lambda*z2*T/2);
+    z3 = e1-e2;
+    z4 = z1*e2-z2*e1;
+    z5 = z1*e1-z2*e2;
+
+    if abs(z0)<1e-5
+        z6 = lambda*T;
+        z7 = 2;
+        z8 = 2*(1+lambda*s0*T);
+        z9 = lambda*T^2/2;
+        z10 = 2*T;
+        z11 = 2*T+lambda*s0*T^2;
+    else
+        z6 = z3/z0;
+        z7 = z4/z0;
+        z8 = z5/z0;
+        if lambda==0
+            z9 = 0;
+            z10 = 2*T;
+            z11 = 2*T+lambda*s0*T^2;
+        else
+            if z1*z2==0
+                if z1==0
+                    z9 = (T-2*(e2-1)/lambda/z2)/z0;
+                    z10 = -z2/z0*T;
+                else
+                    z9 = (2*(e1-1)/lambda/z1-T)/z0;
+                    z10 = z1/z0*T;
+                end
+            else
+                z9 = 2*((e1-1)/z1-(e2-1)/z2)/z0/lambda;
+                z10 = 2*((e2-1)*z1/z2-(e1-1)*z2/z1)/z0/lambda;
+            end
+            z11 = 2*z6/lambda;
+        end
+    end
+
+    if lambda==0
+        a13 = -v*s0*T;
+        a14 = T*c0*w1;
+        a23 = v*c0*T;
+        a24 = T*s0;
+        a34 = -k*c0*T*w1;
+        b11 = c0*T^2/2*w1;
+        b21 = s0*T^2/2;
+        b31 = -k*T^2*c0/2*w1;
+        b12 = -(1+v*T/2/lr)*v*alpha1*s0*T*w1;
+        b22 = (1+v*T/2/lr)*v*T*c0*alpha1;
+        b32 = v*alpha1*T/lr;
+    else
+        if c0==0
+            a14 = 0;
+            a24 = s0*T;
+            a34 = 0;
+            b11 = 0;
+            b21 = s0*T^2/2;
+            b31 = 0;
+        else
+            a14 = (s0*(1-z8/2)+z6)/(v*k*c0);
+            a24 = (-1+s0*c0^2*z6+z7/2)/lambda/c0^2;
+            a34 = (s0*(z8/2-1)-z6)/v/c0;
+            b11 = (s0*(T-z11/2)+z9)/(v*k*c0);
+            b21 = (-T+s0*c0^2*z9+z10/2)/lambda/c0^2;
+            b31 = (s0*(z11/2-T)-z9)/v/c0;
+        end
+        a13 = (1-z8/2)/k;
+        a23 = w0*c0*z6/k;
+        w2 = (w0/k/lr+s0);
+        b12 = (-T*s0+c0^2*z9+(T-z11/2)*w2)*v*alpha1*w1;
+        b22 = (z10/2+z9*w2)*v*c0*alpha1;
+        b32 = (z11*w2/2-z9*c0^2)*lambda*alpha1;
+    end
+
+    Ad = [1,    c0*w1*z6,    a13,         a14;
+          0,    z7/2,             a23,         a24;
+          0,    -k*c0*z6*w1, z8/2,   a34;
+          0,    0,                    0,          1];
+
+    Bd = [b11,   b12;
+          b21,   b22;
+          b31,   b32;
+          T,    0];
+      
+    q1 = 1/(1-k*x(2));    %   1/(1-k*d)
+    q2 = cos(x(3));        %   cos(phi)
+    q3 = x(4)*q2*q1;       %   s_dot = v*cos(phi)/(1-k*d)
+    fcq = [q3; x(4)*sin(x(3)); -k*q3; 0];
+    x0fcT = x+T*fcq;
+end
